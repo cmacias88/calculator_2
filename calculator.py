6b21c630b9f8441d1dc8a5142e0d25d0e6a85848
Chloe Macias <chloeamacias@gmail.com>
2022-08-10 00:31:17 -0400

    Check Calculator Input

--- calculator.py
+++ calculator.py
@@ -3,13 +3,37 @@
 from arithmetic import (add, subtract, multiply, divide, square, cube,
                         power, mod, )
 
+print("This is a calculator.")
+print()
+print("It does the following operations:") 
+print("add(+)\nsubtract(-)\nmultiply(*)\ndivide(/)\nsquare(square)\ncube(cube)\npower(pow)\nmodulus(mod)\nquit(q)\n")
+calculator_operations = ['+', '-', '*', '/', 'square', 'cube', 'pow', 'mod','q']
+
+def calculator_input_validation(calculator_input):
+    while True:
+        calculator_input = calculator_input.split(" ")
+        if calculator_input[0] in calculator_operations:
+            if len(calculator_input) > 1:
+                if (len(calculator_input)==2 and calculator_input[1].isdigit()) or (len(calculator_input)==3 and calculator_input[1].isdigit() and calculator_input[2].isdigit()):
+                    return calculator_input
+                else:
+                    print("Please enter numbers after your operation.")
+                    calculator_input = input("> ")
+            elif calculator_input[0]=='q':
+                return calculator_input
+            else:
+                print("You have not input values to calculate.")
+                calculator_input = input("> ")
+        else:
+            print("You have not entered a valid operation.")
+            calculator_input = input("> ") 
+
 while True:
     calculator_input = input("Please enter math function followed by the two numbers you would like to use (Please separate items by spaces): ")
-
-    if calculator_input == "q":
+    calculator_input = calculator_input_validation(calculator_input)
+    if calculator_input[0] == "q":
         break
     else:
-        calculator_input = calculator_input.split(" ") 
         num_1 = int(calculator_input[1])
         if len(calculator_input) > 2:
             num_2 = int(calculator_input[2]) 
